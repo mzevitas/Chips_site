cae32ea29c282b95cccbd1abe4011e859db0be89
Mike Zevitas <mzevitas@gmail.com>
2015-09-27 14:38:07 -0400

    start of new site

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        menu: false,
+        anchors:['firstPage', 'secondPage'],
+        sectionsColor : [],
+        fixedElements: '#header, .footer',
+       
+
+    });
+    });
