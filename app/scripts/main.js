baa6695c6028c5e4a2ab5d1ad5a3a7356d81df4f
Mike Zevitas <mzevitas@gmail.com>
2015-10-29 12:20:34 -0400

    working on Slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,21 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         //Navigation
         menu: false,
-        anchors:['firstPage', 'secondPage'],
+        anchors:['firstPage', 'secondPage', 'thirdPage','fourthPage'],
         sectionsColor : [],
         fixedElements: '#header, .footer',
+        css3: true,
+         scrollingSpeed: 800,
+         scrollBar: false,
+        scrollOverflow: false,
+         // fitToSection: true,
+         resize: false,
+         touchSensitivity: 5,
+         easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
+         sectionsColor : ['#000000','#000000','#000000','#000000',],
+         responsiveHeight: 600,
        
 
     });
     });
+
